3dd0499a1efe2eb0a9236496114df3c2c33455c8
Catalin <boja.catalin@hotmail.com>
2020-11-07 14:13:16 +0200

    Day 3 update

--- Crypto_02/src/ro/ase/ism/sap/SymCipher.java
+++ Crypto_02/src/ro/ase/ism/sap/SymCipher.java
@@ -6,17 +6,24 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.security.AlgorithmParameters;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.KeyFactory;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
 import java.util.concurrent.RecursiveAction;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
 import javax.crypto.IllegalBlockSizeException;
 import javax.crypto.NoSuchPaddingException;
+import javax.crypto.SecretKeyFactory;
 import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.PBEKeySpec;
+import javax.crypto.spec.PBEParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class SymCipher {
@@ -104,7 +111,8 @@ public class SymCipher {
 	}
 
 	public static void encryptCBC(String inputFileName, String password, String outputFileName, String encAlgorithm)
-			throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+			throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
+			IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
 
 		// managing files
 		File inputFile = new File(inputFileName);
@@ -120,49 +128,214 @@ public class SymCipher {
 			outputFile.createNewFile();
 		}
 		FileOutputStream fos = new FileOutputStream(outputFile);
-		
-		//CBC requires an IV value - it's not a secret
-		//option 1 - use predefine value
-		//option 2 - generate a random one
-		
-		//decide how to handle it
-		//option 1 - hardcode the value
-		//option 2 - write it in the cipher text file - at the beginning
-		
-		//we go for a random one that we will write at the beginning of the file
-		
-		//create the Cipher
-		Cipher cipher = Cipher.getInstance(encAlgorithm+"/CBC/PKCS5Padding");
+
+		// CBC requires an IV value - it's not a secret
+		// option 1 - use predefine value
+		// option 2 - generate a random one
+
+		// decide how to handle it
+		// option 1 - hardcode the value
+		// option 2 - write it in the cipher text file - at the beginning
+
+		// we go for a random one that we will write at the beginning of the file
+
+		// create the Cipher
+		Cipher cipher = Cipher.getInstance(encAlgorithm + "/CBC/PKCS5Padding");
 		int blockSize = cipher.getBlockSize();
-		
-		
-		//generate a random IV
+
+		// generate a random IV
 		SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
 		byte[] iv = new byte[blockSize];
 		secureRandom.nextBytes(iv);
-		
-		//init the Cipher
+
+		// init the Cipher
 		SecretKeySpec key = new SecretKeySpec(password.getBytes(), encAlgorithm);
 		IvParameterSpec ivParam = new IvParameterSpec(iv);
 		cipher.init(Cipher.ENCRYPT_MODE, key, ivParam);
-		
-		//write it in the output file
+
+		// write it in the output file
 		fos.write(iv);
-		
+
 		byte[] inputBuffer = new byte[blockSize];
 		byte[] outputBuffer;
 		int noBytes = 0;
-		
-		while((noBytes = fis.read(inputBuffer)) != -1) {
+
+		while ((noBytes = fis.read(inputBuffer)) != -1) {
 			outputBuffer = cipher.update(inputBuffer, 0, noBytes);
 			fos.write(outputBuffer);
 		}
 		outputBuffer = cipher.doFinal();
 		fos.write(outputBuffer);
-		
+
 		fis.close();
 		fos.close();
-		
 	}
 
+	public static void decryptCBC(String inputFileName, String password, String outputFileName, String encAlgorithm)
+			throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
+			IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
+
+		// managing files
+		File inputFile = new File(inputFileName);
+		if (!inputFile.exists()) {
+			System.out.println("---------- The file is not there -------");
+			throw new FileNotFoundException();
+		}
+		FileInputStream fis = new FileInputStream(inputFile);
+
+		File outputFile = new File(outputFileName);
+		if (!outputFile.exists()) {
+			outputFile.createNewFile();
+		}
+		FileOutputStream fos = new FileOutputStream(outputFile);
+
+		// cipher
+		Cipher cipher = Cipher.getInstance(encAlgorithm + "/CBC/PKCS5Padding");
+		int blockSize = cipher.getBlockSize();
+		SecretKeySpec key = new SecretKeySpec(password.getBytes(), encAlgorithm);
+
+		// we know the IV is the first block in the received file
+		byte[] iv = new byte[blockSize];
+		fis.read(iv);
+
+		IvParameterSpec ivParam = new IvParameterSpec(iv);
+
+		cipher.init(Cipher.DECRYPT_MODE, key, ivParam);
+
+		byte[] input = new byte[blockSize];
+		byte[] output;
+		int noBytes = 0;
+
+		while ((noBytes = fis.read(input)) != -1) {
+			output = cipher.update(input, 0, noBytes);
+			fos.write(output);
+		}
+
+		output = cipher.doFinal();
+		fos.write(output);
+
+		fis.close();
+		fos.close();
+	}
+
+	public static PBESpecs encryptPBE(String inputFileName, String password, String outputFileName, String encAlgorithm)
+			throws IOException, NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException,
+			IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException,
+			InvalidKeySpecException {
+
+		// managing files
+		File inputFile = new File(inputFileName);
+		if (!inputFile.exists()) {
+			System.out.println("---------- The file is not there -------");
+			throw new FileNotFoundException();
+		}
+		FileInputStream fis = new FileInputStream(inputFile);
+		BufferedInputStream bis = new BufferedInputStream(fis);
+
+		File outputFile = new File(outputFileName);
+		if (!outputFile.exists()) {
+			outputFile.createNewFile();
+		}
+		FileOutputStream fos = new FileOutputStream(outputFile);
+
+		// init PBE Cipher
+		Cipher cipher = Cipher.getInstance(encAlgorithm);
+		int blockSize = cipher.getBlockSize();
+
+		// create the PBE key based on user string password
+		SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+		byte[] salt = new byte[cipher.getBlockSize()];
+		secureRandom.nextBytes(salt);
+		int noIterations = 1000;
+		PBEKeySpec pbeKey = new PBEKeySpec(password.toCharArray(), salt, noIterations, blockSize);
+
+		SecretKeyFactory keyFactory = SecretKeyFactory.getInstance(encAlgorithm);
+		Key key = keyFactory.generateSecret(pbeKey);
+
+		cipher.init(Cipher.ENCRYPT_MODE, key);
+
+		// print the PBE details
+		System.out.println("Using PBE with the user password " + password);
+		Util.printHex(cipher.getIV(), "PBE generated IV ");
+		Util.printHex(salt, "Random salt ");
+		System.out.println("\nNo iterations " + noIterations);
+		
+	
+		PBESpecs pbeSpecs = 
+				new PBESpecs(salt, noIterations, cipher.getIV(),
+						cipher.getParameters().getEncoded());
+
+		// encryption
+
+		byte[] input = new byte[blockSize];
+		byte[] output;
+		int noBytes = 0;
+
+		while ((noBytes = fis.read(input)) != -1) {
+			output = cipher.update(input, 0, noBytes);
+			fos.write(output);
+		}
+
+		output = cipher.doFinal();
+		fos.write(output);
+
+		fis.close();
+		fos.close();
+
+		return pbeSpecs;
+	}
+
+	public static void decryptPBE(String inputFileName, String password, String outputFileName, String encAlgorithm,
+			PBESpecs pbeSpecs) throws IOException, NoSuchAlgorithmException, NoSuchPaddingException,
+			InvalidKeyException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException,
+			InvalidKeySpecException {
+
+		// managing files
+		File inputFile = new File(inputFileName);
+		if (!inputFile.exists()) {
+			System.out.println("---------- The file is not there -------");
+			throw new FileNotFoundException();
+		}
+		FileInputStream fis = new FileInputStream(inputFile);
+
+		File outputFile = new File(outputFileName);
+		if (!outputFile.exists()) {
+			outputFile.createNewFile();
+		}
+		FileOutputStream fos = new FileOutputStream(outputFile);
+
+		Cipher cipher = Cipher.getInstance(encAlgorithm);
+		int blockSize = cipher.getBlockSize();
+		
+		PBEKeySpec keySpec = new PBEKeySpec(password.toCharArray(), 
+				pbeSpecs.getSalt(), pbeSpecs.getNoIterations(),
+				blockSize);
+		SecretKeyFactory keyFactory = 
+				SecretKeyFactory.getInstance(encAlgorithm);
+		Key key = keyFactory.generateSecret(keySpec);
+		
+		AlgorithmParameters algParams;
+		algParams = AlgorithmParameters.getInstance(encAlgorithm);
+		algParams.init(pbeSpecs.getParams());
+		
+		cipher.init(
+				Cipher.DECRYPT_MODE, key, algParams);
+
+
+		// decryption
+		byte[] input = new byte[blockSize];
+		byte[] output;
+		int noBytes = 0;
+
+		while ((noBytes = fis.read(input)) != -1) {
+			output = cipher.update(input, 0, noBytes);
+			fos.write(output);
+		}
+
+		output = cipher.doFinal();
+		fos.write(output);
+
+		fis.close();
+		fos.close();
+	}
 }
--- Crypto_02/src/ro/ase/ism/sap/TestCrypto.java
+++ Crypto_02/src/ro/ase/ism/sap/TestCrypto.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
@@ -11,7 +12,7 @@ import javax.crypto.NoSuchPaddingException;
 
 public class TestCrypto {
 
-	public static void main(String[] args) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, IOException, InvalidAlgorithmParameterException {
+	public static void main(String[] args) throws InvalidKeyException, NoSuchAlgorithmException, NoSuchPaddingException, IllegalBlockSizeException, BadPaddingException, IOException, InvalidAlgorithmParameterException, InvalidKeySpecException {
 	
 		System.out.println("Starging ...");
 		
@@ -32,6 +33,20 @@ public class TestCrypto {
 		//CBC
 		SymCipher.encryptCBC(
 				"Message.txt", "password12345678", "MessageCBC.enc", "AES");
+		SymCipher.decryptCBC("MessageCBC.enc", "password12345678", 
+				"Message3.txt", "AES");
+		
+		//1 what do you do if the user password is not = with the block size
+		//2 what do you do if the key is a string based on
+		
+		//solution to extend/reduce passwords to required size
+		//solution to convert string based passwords to binary values
+		//PBE - password based encryption
+		
+		PBESpecs pbeSpecs = SymCipher.encryptPBE("Message.txt", "a", 
+				"MessagePBE.enc", "PBEWithHmacSHA256AndAES_128");
+		SymCipher.decryptPBE("MessagePBE.enc", "a", 
+				"Message4.txt", "PBEWithHmacSHA256AndAES_128", pbeSpecs);
 		
 		System.out.println("That's it");
 		
